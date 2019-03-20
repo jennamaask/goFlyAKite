8b357f5cb57698e5956f1e4f395cd3a0cf3c2342
Jenna <jenna.maask@gmail.com>
2019-03-20 12:36:18 -0400

    re-styled langing page

--- src/partials/Header.js
+++ src/partials/Header.js
@@ -1,6 +1,6 @@
 import "../styles/Header.css";
 import React, { Component } from "react";
-import Scrollchor from "react-scrollchor";
+
 
 class Header extends Component {
   render() {
@@ -9,22 +9,6 @@ class Header extends Component {
         <div className="wrapper">
           <h1>Go Fly a Kite</h1>
           <p>Your daily guide to kitesurfing on the great lakes</p>
-          <nav>
-            <ul>
-              <li>
-                {/* plugin for smooth scroll to intakeForm on click of link */}
-                <Scrollchor to="intakeForm">
-                  Tell us about your kiting today
-                </Scrollchor>
-              </li>
-              <li>
-                {/* plugin for smooth scroll to conditions on click of link */}
-                <Scrollchor to="conditions">
-                  See the most recent conditions
-                </Scrollchor>
-              </li>
-            </ul>
-          </nav>
         </div>
       </header>
     );
--- src/partials/Main.js
+++ src/partials/Main.js
@@ -4,6 +4,8 @@ import lakes from "../assets/greatLakes.svg";
 import Form from "./Form.js";
 import Overlay from "./Overlay.js";
 import "../styles/Overlay.css";
+import Scrollchor from "react-scrollchor";
+
 
 class Main extends Component {
   render() {
@@ -30,6 +32,22 @@ class Main extends Component {
               </div>
             </div>
           </div>
+          <nav>
+            <ul>
+              <li>
+                {/* plugin for smooth scroll to intakeForm on click of link */}
+                <Scrollchor to="intakeForm">
+                  Tell us about your kiting today
+                </Scrollchor>
+              </li>
+              <li>
+                {/* plugin for smooth scroll to conditions on click of link */}
+                <Scrollchor to="conditions">
+                  See the most recent conditions
+                </Scrollchor>
+              </li>
+            </ul>
+          </nav>
           {/* passing down scrollTo function to form, to use for onSubmit */}
           <Form scrollTo={this.props.scrollTo} />
         </div>
